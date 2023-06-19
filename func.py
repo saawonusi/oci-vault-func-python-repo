302b72e1684d1cf0d56381e645c7e791d711658d
sawonusi <stephen.awonusi@technipfmc.com>
2023-06-19 13:41:59 +0000

    func.py code update

--- func.py
+++ func.py
@@ -4,11 +4,11 @@ import oci
 import base64
 
 
-def get_secret(ocid):
+def get_secret('ocid1.vaultsecret.oc1.uk-london-1.amaaaaaakujrcpia636am4piaqlzx3x6rhfsavauh5e27yopulvjfbngplca'):
     signer = oci.auth.signers.get_resource_principals_signer()
     try:
         client = oci.secrets.SecretsClient({}, signer=signer)
-        secret_content = client.get_secret_bundle(ocid).data.secret_bundle_content.content.encode('utf-8')
+        secret_content = client.get_secret_bundle('ocid1.vaultsecret.oc1.uk-london-1.amaaaaaakujrcpia636am4piaqlzx3x6rhfsavauh5e27yopulvjfbngplca').data.secret_bundle_content.content.encode('utf-8')
         decrypted_secret_content = base64.b64decode(secret_content).decode('utf-8')
     except Exception as ex:
         logging.getLogger().error("getSecret: Failed to get Secret" + str(ex))
