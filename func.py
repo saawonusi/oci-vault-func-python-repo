88b9a2c484e17e21466488533c95411ad5cc0c5e
sawonusi <stephen.awonusi@technipfmc.com>
2023-06-20 14:10:40 +0000

    update

--- func.py
+++ func.py
@@ -3,14 +3,27 @@ import logging
 import oci
 import base64
 
+# Replace secret_id value below with the ocid of your secret
+secret_id = "ocid1.vaultsecret.oc1.uk-london-1.amaaaaaakujrcpia636am4piaqlzx3x6rhfsavauh5e27yopulvjfbngplca"
 
-def get_secret(ocid1.vaultsecret.oc1.uk-london-1.amaaaaaakujrcpia636am4piaqlzx3x6rhfsavauh5e27yopulvjfbngplca):
-    signer = oci.auth.signers.get_resource_principals_signer()
-    try:
-        client = oci.secrets.SecretsClient({}, signer=signer)
-        secret_content = client.get_secret_bundle(ocid1.vaultsecret.oc1.uk-london-1.amaaaaaakujrcpia636am4piaqlzx3x6rhfsavauh5e27yopulvjfbngplca).data.secret_bundle_content.content.encode('utf-8')
-        decrypted_secret_content = base64.b64decode(secret_content).decode('utf-8')
-    except Exception as ex:
-        logging.getLogger().error("getSecret: Failed to get Secret" + str(ex))
-        raise
-    return decrypted_secret_content
+# By default this will hit the auth service in the region the instance is running.
+signer = oci.auth.signers.get_resource_principals_signer()
+
+# In the base case, configuration does not need to be provided as the region and tenancy are obtained from the InstancePrincipalsSecurityTokenSigner
+identity_client = oci.identity.IdentityClient(config={}, signer=signer)
+
+# Get instance principal context
+secret_client = oci.secrets.SecretsClient(config={}, signer=signer)
+
+# Retrieve secret
+def read_secret_value(secret_client, secret_id):
+    response = secret_client.get_secret_bundle(secret_id)
+    base64_Secret_content = response.data.secret_bundle_content.content
+    base64_secret_bytes = base64_Secret_content.encode('ascii')
+    base64_message_bytes = base64.b64decode(base64_secret_bytes)
+    secret_content = base64_message_bytes.decode('ascii')
+    return secret_content
+
+# Print secret
+secret_contents = read_secret_value(secret_client, secret_id)
+print(format(secret_contents))
